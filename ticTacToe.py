280bfc45dd7d270d141c79ffdd5db36f6ac4da5c
FlynnML <flynn.michael-legg@students.olin.edu>
2018-03-09 15:03:37 -0500

    Fixed bugs inbasic  tiTacToe, now onto full extreme mode!

--- ticTacToe.py
+++ ticTacToe.py
@@ -6,22 +6,24 @@ class Grid():
     def __init__(self, listy=[[None,None,None], \
                               [None,None,None], \
                               [None,None,None]],
-                              xo='X', winner = None):
+                              xo='X', winner = None, counter = 0):
         #self.background = background
         self.listy = listy
         self.xo = xo
         self.winner = winner
+        self.counter = counter
 
-    def make_board(self,board_size):
+    def make_board(self,board_size,board):
         background = pygame.Surface(board_size.get_size())
         background = background.convert()
-        background.fill(255,255,255)
+        #background.fill(255,255,255)
         #vert
-        pygame.draw.line (background, (0,0,0),(100,0),(100,300), 2)
-        pygame.draw.line (background, (0,0,0),(200,0),(200,300), 2)
+        pygame.draw.line(board, (0,0,0),(100,0),(100,300), 5)
+        pygame.draw.line(board, (0,0,0),(200,0),(200,300), 5)
         #horiz
-        pygame.draw.line (background, (0,0,0),(0,100),(300,100), 2)
-        pygame.draw.line (background, (0,0,0),(0,200),(300,200), 2)
+        pygame.draw.line(board, (0,0,0),(0,100),(300,100), 5)
+        pygame.draw.line(board, (0,0,0),(0,200),(300,200), 5)
+        pygame.display.update()
 
         return background
 
@@ -36,7 +38,7 @@ class Grid():
         board.fill ((250,250,250), (0,300,300,25))
         board.blit(text, (10,300))
 
-    def showBoard(self,board_size, board):
+    def showBoard(self,board_size,board):
         self.status(board)
         board_size.blit(board, (0,0))
         pygame.display.flip()
@@ -60,12 +62,12 @@ class Grid():
         xCenter = ((bColumn)*100)+50
         yCenter = ((bRow)*100)+50
         if (player_piece == 'O'):
-            pygame.draw.circle(board, (0,0,0),(xCenter,yCenter),44,2)
+            pygame.draw.circle(board, (0,0,250),(xCenter,yCenter),30,12)
         else:
-            pygame.draw.line(board,(0,0,0),(xCenter-22,yCenter-22), \
-                                   (xCenter+22,yCenter+22),2)
-            pygame.draw.line(board,(0,0,0),(xCenter+22,yCenter+22), \
-                                   (xCenter-22,yCenter-22),2)
+            pygame.draw.line(board,(255,215,0),(xCenter-22,yCenter-22), \
+                                   (xCenter+22,yCenter+22),12)
+            pygame.draw.line(board,(255,215,0),(xCenter-22,yCenter+22), \
+                                   (xCenter+22,yCenter-22),12)
         self.listy[bRow][bColumn] = player_piece
 
     def clickedy(self,board):
@@ -83,45 +85,53 @@ class Grid():
     def winnerwinnerchickendinner(self,board):
         for row in range(0,3):
             if self.listy[row][0] == self.listy[row][1] == self.listy[row][2] \
-               and self.listy[row][0] != None:
+               and self.listy[row][0] != None and self.counter == 0:
                self.winner = self.listy[row][0]
-               pygame.draw.line (board, (250,0,0), (0,(row+1)*100 -50), (300,(row+1)*100 - 50),2)
+               self.counter += 1
+               pygame.draw.line (board, (250,0,0), (0,(row+1)*100 -50), (300,(row+1)*100 - 50),6)
                break
         for column in range(0,3):
             if self.listy[0][column] == self.listy[1][column] == self.listy[2][column] \
-               and self.listy[0][column] != None:
+               and self.listy[0][column] != None and self.counter == 0:
                self.winner = self.listy[0][column]
-               pygame.draw.line (board, (250,0,0), ((column+1)*100 -50, 0), ((column+1)*100 - 50, 300),2)
+               self.counter += 1
+               pygame.draw.line (board, (250,0,0), ((column+1)*100 -50, 0), ((column+1)*100 - 50, 300),6)
                break
         if self.listy[0][0] == self.listy[1][1] == self.listy[2][2] and \
-           self.listy[0][0] != None:
+           self.listy[0][0] != None and self.counter == 0:
             # diagonal left to right
             self.winner = self.listy[0][0]
-            pygame.draw.line (board, (250,0,0), (50, 50), (250, 250), 2)
+            self.counter += 1
+            pygame.draw.line (board, (250,0,0), (0, 0), (300, 300), 6)
 
         if self.listy[0][2] == self.listy[1][1] == self.listy[2][0] and \
-           self.listy[0][2] != None:
+           self.listy[0][2] != None and self.counter == 0:
             # diagonal right to left
             self.winner = self.listy[0][2]
-            pygame.draw.line (board, (250,0,0), (250, 50), (50, 250), 2)
+            self.counter += 1
+            pygame.draw.line (board, (250,0,0), (300, 0), (0, 300), 6)
 
 #yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 simple = Grid()
 
 pygame.init()
 board_size = pygame.display.set_mode ((300, 325))
-#simple.make_board(board_size)
+# create the game board
+board = board_size
+# simple.make_board(board_size, board)
+pygame.display.update()
+simple.showBoard(board_size, board_size)
+pygame.display.update()
 #white = (255,255,255)
 board_size.fill([255, 255, 255])
 pygame.display.set_caption ('Tic Tac Toe')
 pygame.display.flip()
-# create the game board
-board = board_size
 
 # main event loop
 running = 1
 
 while (running == 1):
+    simple.make_board(board_size, board)
     for event in pygame.event.get():
         if event.type is QUIT:
             running = 0
